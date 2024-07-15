908d343d0de616543bf1dfabe99e3e96ddac3a8d
Saugat Rajbhandari <saugatrajbhandari47@gmail.com>
2024-07-15 21:42:44 +0545

    feat: add coursel and active navlink

---
+++ staticfiles/script.js
@@ -0,0 +1,27 @@
+console.log("hello");
+
+const serviceLink = document.getElementById("serviceId");
+const homeLink = document.getElementById("homeId");
+const aboutLink = document.getElementById("aboutId");
+const contactLink = document.getElementById("contactId");
+const bookLink = document.getElementById("bookId");
+const registerLink = document.getElementById("registerId");
+
+if (window.location.pathname === "/") {
+  homeLink.style.color = "red";
+}
+if (window.location.pathname === "/service/") {
+  serviceLink.style.color = "red";
+}
+if (window.location.pathname === "/aboutus/") {
+  aboutLink.style.color = "red";
+}
+if (window.location.pathname === "/contact/") {
+  contactLink.style.color = "red";
+}
+if (window.location.pathname === "/table/") {
+  bookLink.style.color = "red";
+}
+if (window.location.pathname === "/register/") {
+  registerLink.style.color = "red";
+}
